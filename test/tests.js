25587d93ff47502c0be0306fe02bef7f24c87ce1
AliOgretmen <aliogretmen.ch@gmail.com>
2017-09-18 01:04:01 +0200

    first commit (addition&substract)

--- test/tests.js
+++ test/tests.js
@@ -6,9 +6,22 @@
 
 describe('Test Suite - Test your own functions with edge cases', () => {
 
-    it('a function.', function(){
-        let result = 1;
-        result.should.not.be.equal(2);
+    it('parse https://it-club-oberland.ch#section.', function(){
+        let analysis = analyseUrl('https://it-club-oberland.ch#section');
+        
+        analysis.schema.should.be.equal('https');
+        analysis.host.should.be.equal('it-club-oberland.ch');
+        analysis.fragment.should.be.equal('section');
     });
       
+    
+    it('parse http://localhost:8080/api?sort=ASC', function(){
+        let analysis = analyseUrl('http://localhost:8080/api?sort=ASC');
+        
+        analysis.schema.should.be.equal('http');
+        analysis.host.should.be.equal('localhost');
+        analysis.port.should.be.equal('8080');
+        analysis.path.should.be.equal('api');
+        analysis.query.should.be.equal('sort=ASC');
+    });
 });
